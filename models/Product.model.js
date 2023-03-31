d4ca12680887f5e2a755ef382c681685955eca84
Andrea <ironhack@Ironhacks-MacBook-Pro-2.local>
2023-03-31 16:52:18 +0200

    prueba de accesos

--- models/Product.model.js
+++ models/Product.model.js
@@ -1,60 +1,58 @@
-const mongoose = require('mongoose');
-const { REQUIRED_FIELD } = require('../config/errorMessages');
+const mongoose = require("mongoose");
+const { REQUIRED_FIELD } = require("../config/errorMessages");
 
 const productSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, REQUIRED_FIELD]
+      required: [true, REQUIRED_FIELD],
     },
     description: {
       type: String,
-      required: [true, REQUIRED_FIELD]
+      required: [true, REQUIRED_FIELD],
     },
     initialprice: {
       type: Number,
-      required: [true, REQUIRED_FIELD]
+      required: [true, REQUIRED_FIELD],
     },
     state: {
       type: String,
-      enum: ['new', 'almost new', 'used'],
-      required: [true, REQUIRED_FIELD]
+      enum: ["new", "almost new", "used"],
+      required: [true, REQUIRED_FIELD],
     },
     image: {
       type: [String],
-      required: [true, REQUIRED_FIELD]
+      required: [true, REQUIRED_FIELD],
     },
     user: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
-      required: [true, REQUIRED_FIELD]
+      ref: "User",
+      required: [true, REQUIRED_FIELD],
     },
     category: {
+      //quizás un modelo de categoría
       type: String,
-      enum: ['Clothes', 'XXXXX', 'XXXXX'],  //pensaaaaaaaaaaaaaaaaaaaaaaar y preguntar como hacer subcategorias
-      required: [true, REQUIRED_FIELD]
+      enum: ["Clothes", "XXXXX", "XXXXX"], //pensaaaaaaaaaaaaaaaaaaaaaaar y preguntar como hacer subcategorias
+      required: [true, REQUIRED_FIELD],
     },
     shipment: {
-        type: String,
-        enum: ["Yes", "No"],
-        default: "No"
-    }
-
+      type: String,
+      enum: ["Yes", "No"],
+      default: "No",
+    },
   },
   {
-    timestamps: true
+    timestamps: true,
   }
-)
+);
 
-  
-productSchema.virtual('bids', {
-  ref: 'Bid',
-  foreignField: 'product',  //nombrar luego el producto en el esquema del bid
-  localField: '_id',
-  justOne: false
-})
+productSchema.virtual("bids", {
+  ref: "Bid",
+  foreignField: "product", //nombrar luego el producto en el esquema del bid
+  localField: "_id",
+  justOne: false,
+});
 
+const Product = mongoose.model("Product", productSchema);
 
-const Product = mongoose.model('Product', productSchema)
-
-module.exports = Product
+module.exports = Product;
